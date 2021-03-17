3b88fa1c90cacff81560845927241d614f6a3043
barrygg11 <barry01test@gmail.com>
2021-03-17 15:01:06 +0800

    商品價格選項

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -1,9 +1,10 @@
 <html>
     <head>
         <meta charset= "UTF-8">
-        <title>shop</title>
+        <title>商品價目表</title>
     </head>
     <body>
+        商品價目表：
         <table border = "1">
             <tr>
             <td>商品序號</td>
@@ -21,4 +22,33 @@
         @endforeach
         </table>
     </body>
+    <p>購物車：</p>
+    <input type="submit" value="送出"><p>
+    <body>
+        <table border = "1">
+              <tr>
+                <td>商品</td>
+                <td>商品價格</td>
+                <td>數量</td>
+              </tr>
+              <tr>
+                <td>白開水</td>
+                <td>100</td>
+                <td><input type="string" name="amount1" value=""></td>
+              </tr>
+              <tr>
+                <td>果汁</td>
+                <td>150</td>
+                <td><input type="string" name="amount2" value=""></td>
+              </tr>
+              <tr>
+                <td>威士忌</td>
+                <td>10000</td>
+                <td><input type="string" name="amount3" value=""></td>
+              </tr>
+              <tr>
+                <td>可樂</td>
+                <td>50</td>
+                <td><input type="string" name="amount4" value=""></td>
+              </tr>
 </html>
